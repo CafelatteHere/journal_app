cb3f8e33b0978267e381c4e5ba70cd5aac2f4f6b
Latteholic <cafelatte.here@gmail.com>
2022-12-16 01:04:33 +0900

    Added navbar; added some bootstrap to user posts page
    - added bootstrap
    - Modified site navbar
    - added to user posts page:
     --- summary of all posts;
     --- posts displayed with bootstrap cards;
     --- create new post button

--- journal_app/journal/views.py
+++ journal_app/journal/views.py
@@ -5,4 +5,7 @@ from django.contrib.auth.models import User
 # Create your views here.
 def posts_list(request):
   posts_list = Post.objects.all()
-  return render(request, 'journal/my_posts.html', {posts_list: posts_list})
+  context = {
+    'posts_list': posts_list
+  }
+  return render(request, 'journal/my_posts.html', context=context)
