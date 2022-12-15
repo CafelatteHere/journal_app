49de9a1c59247da8eb065bf90184db7a64006fbe
Latteholic <cafelatte.here@gmail.com>
2022-12-15 23:51:52 +0900

    updated admin.py
    - registered Post model
    - changed header, title, index title names for Django Admin

--- journal_app/journal/admin.py
+++ journal_app/journal/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+admin.site.header = "Journal app"
+admin.site.site_title = "Journal app"
+admin.site.index_title = "Manage Journal app"
+
+admin.site.register(Post)
