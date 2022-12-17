615882f0e5ecbadbbf5602ed381eed795bfa81eb
Latteholic <cafelatte.here@gmail.com>
2022-12-17 23:49:20 +0900

    fixing the issue with secret_key

--- journal_app/journal_app/settings.py
+++ journal_app/journal_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!vxg$1f#+h)1oq1_9vaz!97g)27ijcaxqo@ww81vd2=s3+#g@p'
+SECRET_KEY =config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
