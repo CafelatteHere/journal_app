f5241c85d5b1819c9e2ede2d8f42ef6f1512cfc2
Latteholic <cafelatte.here@gmail.com>
2022-12-18 21:57:36 +0900

    Updated Post Model and Posts List html page
    Post Model:
    - added 'private' boolean field
    
    Posts List html page:
    - made 'private' field value
    - edited button for creating new post

--- journal_app/journal/models.py
+++ journal_app/journal/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+RUBRIC_CHOICES = [
+  ("No rubric", "No rubric"),
+  ("Daily life", "Daily life"),
+  ("Travel", "Travel"),
+  ("Sport", "Sport"),
+  ("News", "News"),
+  ("Food", "Food"),
+  ("Discussion", "Discussion"),
+  ("Other", "Other")
+]
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=500)
@@ -10,3 +20,8 @@ class Post(models.Model):
   image = models.CharField(max_length=500, null=True, blank=True)
   audio = models.CharField(max_length=500, null=True, blank=True)
   video = models.CharField(max_length=500, null=True, blank=True)
+  rubric = models.CharField(max_length=100, default="No rubric", choices=RUBRIC_CHOICES)
+  private = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.title
